b39840698ff9ac7ef6e1913b543dae3e9aebba14
JorgeSaicoski <jorgesaicoski@gmail.com>
2023-08-26 20:17:38 -0300

    next auth with base UI and routes/api

--- client/src/api/auth/[...nextauth]/route.ts
+++ client/src/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
+import User from "@/models/User";
+import NextAuth from "next-auth/next";
+import bcrypt from "bcryptjs";
+
 
 const handler = NextAuth({
     providers:[
