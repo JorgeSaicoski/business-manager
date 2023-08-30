f43aa4443e3a8dc64c74ed53430f47f4280a2353
JorgeSaicoski <jorgesaicoski@gmail.com>
2023-08-30 20:43:19 -0300

    pre react bulma components

--- client/src/app/components/Auth/LoginButton/LoginButton.tsx
+++ client/src/app/components/Auth/LoginButton/LoginButton.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { useSession } from "next-auth/react";
+import Link from 'next/link';
 
 
 
-const ProjectCard: React.FC = () => {
+const LoginButton: React.FC = () => {
     const { data } = useSession()
     return (
         <div className="buttons">
-            {data ? (
-                <div className="button is-success">
-                    <span>Welcome, !</span>
-                    <button className="button is-light">Logout</button>
+            <div className="modal">
+                <div className="modal-background"></div>
+                <div className="modal-content">
                 </div>
+                <button className="modal-close is-large" aria-label="close"></button>
+            </div>
+            {data ? (
+                    <button className="button is-danger">Logout</button>
             ) : (
-                <button className="button is-info">Login</button>
+                <Link href="/"><button className="button is-info">Login</button></Link>
             )}
         </div>
     );
 };
 
-export default ProjectCard;
+export default LoginButton;
 
 
