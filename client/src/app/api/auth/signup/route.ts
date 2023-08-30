ad027beae622e17d311a5c841c6808ed01bcc56a
JorgeSaicoski <jorgesaicoski@gmail.com>
2023-08-29 23:04:28 -0300

    connect db fixxed

--- client/src/app/api/auth/signup/route.ts
+++ client/src/app/api/auth/signup/route.ts
@@ -1,8 +1,9 @@
-import { connectDB } from "@/db/connect";
-import User from "@/db/models/user";
-import { NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
-import mongoose from "mongoose";
+import { connectDB } from '@/db/connect';
+import User from '@/db/models/user';
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcryptjs';
+
+const MIN_PASSWORD_LENGTH = 6;
 
 export async function POST(request: Request) {
   try {
@@ -10,44 +11,42 @@ export async function POST(request: Request) {
 
     const { fullname, email, password } = await request.json();
 
-    if (password < 6)
+    if (password.length < MIN_PASSWORD_LENGTH) {
       return NextResponse.json(
-        { message: "Password must be at least 6 characters" },
+        { message: 'Password must be at least 6 characters' },
         { status: 400 }
       );
+    }
 
     const userFound = await User.findOne({ email });
 
-    if (userFound)
+    if (userFound) {
       return NextResponse.json(
-        {
-          message: "Email already exists",
-        },
-        {
-          status: 409,
-        }
+        { message: 'Email already exists' },
+        { status: 409 }
       );
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 12);
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const user = new User({
-      fullname,
-      email,
-      password: hashedPassword,
-    });
-
-    const savedUser = await user.save();
-    //console.log(savedUser);
-
-    return NextResponse.json(
-      {
+      const user = new User({
         fullname,
         email,
-        createdAt: savedUser.createdAt,
-        updatedAt: savedUser.updatedAt,
-      },
-      { status: 201 }
-    );
+        password: hashedPassword,
+      });
+
+      const savedUser = await user.save();
+      console.log(savedUser);
+
+      return NextResponse.json(
+        {
+          fullname,
+          email,
+          createdAt: savedUser.createdAt,
+          updatedAt: savedUser.updatedAt,
+        },
+        { status: 201 }
+      );
+    }
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
       return NextResponse.json(
--- client/src/db/connect.ts
+++ client/src/db/connect.ts
@@ -1,20 +1,36 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const { MONGODB_URI } = process.env;
 
 if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI must be defined");
+  throw new Error('MONGODB_URI must be defined');
 }
 
-export const connectDB = async () => {
+mongoose.set('strictQuery', false);
+
+export const connectDB = async (): Promise<void> => {
   try {
-    const { connection } = await mongoose.connect(MONGODB_URI);
-    if (connection.readyState === 1) {
-      console.log("MongoDB Connected");
-      return Promise.resolve(true);
-    }
+    await mongoose.connect(MONGODB_URI, {
+      user: 'admin',
+      pass: '3^9r4$f$o7k*TSk9rJBWbh',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('-----------------------');
+    console.log('MONGO CONNECTED');
+    console.log(mongoose.connection.host);
+    console.log(`${MONGODB_URI}`);
+    console.log('-----------------------');
   } catch (error) {
     console.error(error);
-    return Promise.reject(error);
   }
 };
+
+
+
+
+
+
+
+
